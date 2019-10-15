813faf24d00e5376e54c63a06c447e1aa9233167
jmmaltos2 <jmmaltos2@alaska.edu>
2019-10-15 10:53:52 -0800

    added box.hpp file to repo

---
+++ ProjectHW1/ProjectHW1/Box.hpp
@@ -0,0 +1,21 @@
+/*
+   Box.hpp
+   By: Jared Maltos
+   Created: 10/13/2019
+   CS 202: Homework 01
+   Purpose: .hpp file for Box
+   Details: Put Box.hpp, Box.cpp, and boxtest.cpp into a project to work
+*/
+
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include <iostream>
+#include <string>
+using std::string;
+
+#include <ostream>
+using std::ostream;
+
+
+//class box
